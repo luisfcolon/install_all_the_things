82b7fa299e9c45caea5a8d8ba8873c333a0a95f6
Luis Colon <luiscolon62@gmail.com>
2019-01-05 18:41:56 -0500

    Create fresh_installs.sh

---
+++ fresh_installs.sh
@@ -0,0 +1,50 @@
+if ! command -v brew > /dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install cask
+
+brew cask install \
+  docker \
+  postman \
+  iterm2 \
+  caffeine \
+  dropbox \
+  firefox \
+  slack \
+  spotify \
+  alfred \
+  openoffice \
+  google-chrome \
+  macdown \
+  cyberduck \
+  visual-studio-code \
+  sublime-text \
+  homebrew/cask-versions/java8 \
+  vlc
+  
+brew install \  
+  dos2unix \
+  git \
+  node \
+  yarn \
+  awscli \
+  jq \
+  docker-machine \
+  imagemagick \
+  mcrypt \
+  openssl \
+  python3 \
+  redis \
+  ssh-copy-id \
+  wget \
+  htop \
+  aircrack-ng \
+  coreutils \
+  elasticsearch \
+  go \
+  rbenv \
+  yarn \
+  sass/sass/sass
+  
+npm install -g sass
